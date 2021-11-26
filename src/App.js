6ee9886b90b22867f0df5a098a1d4db4dd6b9ee7
fuad221 <fuad221@outlook.com>
2021-11-26 11:27:20 +0100

    i finished the Home.js and Home.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { Navbar } from './Reference-import'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar, Home } from './Reference-import'
+// import Navbar from './components/Navbar'
+// import Home from './components/Pages/Home'
+
 import './App.css';
 
 function App() {
   return (
     <div>
       <Router>
-        <Navbar />
+        <>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+        </>
       </Router>
     </div>
   );
--- src/Reference-import.js
+++ src/Reference-import.js
@@ -1,17 +1,19 @@
-export  {default as Navbar } from './components/Navbar';
-export  {default as Contact } from './components/Pages/Contact';
-export  {default as Home } from './components/Pages/Home';
-export  {default as Menu } from './components/Pages/Menu';
+export { default as Navbar } from './components/Navbar';
+export { default as Home } from './components/Pages/Home';
+export { default as About } from './components/Pages/About';
+export { default as Menu } from './components/Pages/Menu';
+export { default as Contact } from './components/Pages/Contact';
 
 
 
 
 
 // CSS
-export  {default as ContactCss } from './styles/Contact.css';
-export  {default as HomeCss } from './styles/Home.css';
-export  {default as MenuCss } from './styles/Menu.css';
 export  {default as NavbarCss } from './styles/Navbar.css';
+export  {default as HomeCss } from './styles/Home.css';
+export  {default as AboutCss } from './styles/Home.css';
+export  {default as MenuCss } from './styles/Menu.css';
+export  {default as ContactCss } from './styles/Contact.css';
 
 
 
